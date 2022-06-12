ad6668f57529003a648c9ea9019c91de46906526
Muaz MEMİŞ <muazmemis@gmail.com>
2022-06-12 19:49:56 +0300

    Hibernate - Ders 20 bitti.

--- Hibernate/hiberEx1/src/main/java/app/PojoApp.java
+++ Hibernate/hiberEx1/src/main/java/app/PojoApp.java
@@ -6,12 +6,19 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class PojoApp {
     public static void main(String[] args) {
+
+        Date tarih = new Date();
+
         Pojo pojo = new Pojo();
         pojo.setAdi("Muaz");
         pojo.setDeger(new BigDecimal("5.12"));
+        pojo.setTarih(tarih);
+        pojo.setSaat(tarih);
+        pojo.setTarihSaat(tarih);
 
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
--- Hibernate/hiberEx1/src/main/java/domain/Pojo.java
+++ Hibernate/hiberEx1/src/main/java/domain/Pojo.java
@@ -2,6 +2,7 @@ package domain;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Table(name = "POJO_TABLE")
@@ -19,6 +20,18 @@ public class Pojo {
 
     @Column(name = "DEGER", precision = 15, scale = 2)
     private BigDecimal deger;
+
+    @Column(name = "TARIH")
+    @Temporal(TemporalType.DATE)
+    private Date tarih;
+
+    @Column(name = "SAAT")
+    @Temporal(TemporalType.TIME)
+    private Date saat;
+
+    @Column(name = "TARIH_SAAT")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date tarihSaat;
 
     public Long getId() {
         return id;
@@ -42,5 +55,29 @@ public class Pojo {
 
     public void setDeger(BigDecimal deger) {
         this.deger = deger;
+    }
+
+    public Date getTarih() {
+        return tarih;
+    }
+
+    public void setTarih(Date tarih) {
+        this.tarih = tarih;
+    }
+
+    public Date getSaat() {
+        return saat;
+    }
+
+    public void setSaat(Date saat) {
+        this.saat = saat;
+    }
+
+    public Date getTarihSaat() {
+        return tarihSaat;
+    }
+
+    public void setTarihSaat(Date tarihSaat) {
+        this.tarihSaat = tarihSaat;
     }
 }
