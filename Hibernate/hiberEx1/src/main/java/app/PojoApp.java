3ee27b951b5557b078e1bdf1b3cffbd9ef1ee9d5
Muaz MEMİŞ <muazmemis@gmail.com>
2022-06-12 19:44:25 +0300

    Hibernate - Ders 19 bitti.

--- Hibernate/hiberEx1/src/main/java/app/PojoApp.java
+++ Hibernate/hiberEx1/src/main/java/app/PojoApp.java
@@ -5,9 +5,13 @@ import hibernate.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import java.math.BigDecimal;
+
 public class PojoApp {
     public static void main(String[] args) {
         Pojo pojo = new Pojo();
+        pojo.setAdi("Muaz");
+        pojo.setDeger(new BigDecimal("5.12"));
 
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
--- Hibernate/hiberEx1/src/main/java/domain/Pojo.java
+++ Hibernate/hiberEx1/src/main/java/domain/Pojo.java
@@ -1,16 +1,24 @@
 package domain;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
-@Table(name="POJO_TABLE")
+@Table(name = "POJO_TABLE")
 public class Pojo {
 
     @TableGenerator(name = "table", table = "POJO_SEQ_TABLE", allocationSize = 1,
             pkColumnName = "PK_NAME", valueColumnName = "DEGER", pkColumnValue = "POJO")
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE, generator = "table")
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "ADI", length = 100, nullable = false) // insertable = false, updatable = false
+    private String adi;
+
+    @Column(name = "DEGER", precision = 15, scale = 2)
+    private BigDecimal deger;
 
     public Long getId() {
         return id;
@@ -18,5 +26,21 @@ public class Pojo {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    public BigDecimal getDeger() {
+        return deger;
+    }
+
+    public void setDeger(BigDecimal deger) {
+        this.deger = deger;
     }
 }
