d0e35262487f706b6fc0bf4f8863920f58117035
Muaz MEMİŞ <muazmemis@gmail.com>
2022-06-12 19:23:10 +0300

    Hibernate - Ders 17 bitti.

--- Hibernate/hiberEx1/src/main/java/domain/Pojo.java
+++ Hibernate/hiberEx1/src/main/java/domain/Pojo.java
@@ -6,8 +6,10 @@ import javax.persistence.*;
 @Table(name="POJO_TABLE")
 public class Pojo {
 
+    // allocationSize = artış miktarı, initialValue = başlangıç
+    @SequenceGenerator(name="seq", sequenceName = "POJO_TABLE_ID_SEQ", allocationSize = 1, initialValue = 100)
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
     private Long id;
 
     public Long getId() {
